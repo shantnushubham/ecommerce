95b76b099821ab5413147b4be56fa938199f96ed
pv-912 <prashantverma1223@gmail.com>
2020-04-05 15:33:38 +0530

    service for user

--- routes/user/routes.js
+++ routes/user/routes.js
@@ -14,7 +14,13 @@ var corsOptions = require('../config/cors')
 router.use('/', cors(corsOptions))
 
 // -> /api/user
-router.get('/getUserById/:id', UserMiddleware.verifyUser, userControl.getUserById)
+router.get('/getUserById', UserMiddleware.verifyUser, userControl.getUserById)
+router.post('/addUserAddress', UserMiddleware.verifyUser, userControl.addUserAddress)
+router.put('/updateUserData', UserMiddleware.verifyUser, userControl.updateUserData)
+router.put('/makeDefaultAddress', UserMiddleware.verifyUser, userControl.makeAdressToDefaultAddress)
+router.put('/updateAddress', UserMiddleware.verifyUser, userControl.updateUserAddress)
+router.delete('/deleteUser', UserMiddleware.verifyUser, userControl.deleteUserById)
+router.delete('/deleteAddress', UserMiddleware.verifyUser, userControl.deleteAddress)
 
 
 
