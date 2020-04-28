fab49937407573f917d13008ef79f141d8784a28
= shantnushubham <= shantanu0707@gmail.com>
2020-04-28 20:45:52 +0530

    [Change] Order Module

--- models/Orders/Order.js
+++ models/Orders/Order.js
@@ -7,73 +7,54 @@ var mongooseHistory = require('mongoose-history')
 // 1. approved
 // 2: dispatched
 // 3: placed
-// 4:cancelled
+// 4: cancelled
 
 var orderSchema  = new mongoose.Schema({
     uid:{
         type:String
     },
+
     orderId: {
-        type: String
+        type: String,
+        default: shortid.generate
     },
-    // iid:{
-    //     type: String,
-    //     required: true,
-    //     default:shortid.generate
-    // },
-    // quantity:{
-    //     type:Number,
-    //     required:true
-    // },
 
     orderedItems: [
-        new Schema (
-            {
-                // item: ObjectId,
-                iid: String,
-                quantity: Number
-            }
-        )
+        {
+            quantity: Number,
+            items: { 
+                type : String, 
+            },
+        }
     ],
+
     deliveryAddress:{
-        type:mongoose.Types.ObjectId,
-        ref: 'delivery_address',
+        type: String,
         required: true
     },
+
     purchaseTime:{
         type: Date,
         required: true,
         default: Date.now
     },
+
     status:{
         type: Number,
         required: true
     },
-    estimatedDeliveryDate:{
-        type: Date,
-    },
-    deliveredDate:{
-        type: Date,
-    },
-    approvedDate:{
-        type: Date,
-    },
-    isPaymentOnline: {
-        type: Boolean,
-        required: true
-    },
-    isPaymentOffline: {
-        type: Boolean,
-        required: true
-    },
-    isOnlinePaymentSuccessful : {
-        type: Boolean,
-        required: true,
-        default: false,
-    },
-    transaction_id:{
-        type: String
-    }
+
+    // estimatedDeliveryDate:{
+    //     type: Date,
+    // },
+
+    // deliveredDate:{
+    //     type: Date,
+    // },
+
+    // approvedDate:{
+    //     type: Date,
+    // },
 });
 
 orderSchema.plugin(mongooseHistory)
