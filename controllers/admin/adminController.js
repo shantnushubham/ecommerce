eceb0e323f92109a75f5e7dfafe64573e6620922
prakharshriv <prakharshriv@gmail.com>
2020-11-10 22:42:39 +0530

    user download

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -370,7 +370,21 @@ exports.downloadInvoiceByRangeShipment = function (req, res) {
 }
 
 exports.downloadUserList = function (req, res) {
-    userModel.find({}, function (err, foundUsers) {
+   var filter={
+
+   }
+   if(req.params.type=="individual")
+   {
+       filter.isBusiness=false
+       filter.premium=false
+   }
+   if(req.params.type==="business")
+   filter.isBusiness=true
+   if(req.params.type==="premium")
+   {filter.isBusiness=true
+    filter.premium=true
+}
+    userModel.find(filter, function (err, foundUsers) {
         console.log(foundUsers);
         if (err) {
             req.flash('error', 'error in downloading')
