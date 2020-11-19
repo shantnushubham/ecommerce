6e62a7f1731144b890865f09e6dde46e1874eb83
shantnushubham <shantanu0707@gmail.com>
2020-11-19 12:15:39 +0530

    [Bug fixes]

--- routes/items.js
+++ routes/items.js
@@ -2,7 +2,7 @@ var express = require('express');
 var itemController = require('../controllers/item/itemController')
 var app = express();
 app.get('/items/category/:category', itemController.categoryPages)
-app.get("/items/:category", itemController.getItemsByCategoryAndSubCategory)
+app.get("/get-items/:category", itemController.getItemsByCategoryAndSubCategory)
 
 app.get('/items', itemController.getAllItems)
 app.get('/items/:iid', itemController.getItem)
